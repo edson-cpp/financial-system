191155331c5fd28ba0127359124155eac56ad64f
Edson Aguiar <edson_vc@yahoo.com.br>
2026-05-17 03:11:38 -0300

    Fix IP and Port

--- main.cc
+++ main.cc
@@ -4,8 +4,5 @@
 int main() {
     Database::initialize();
     drogon::app().loadConfigFile("/home/edson/financial-system/config.json");
-    drogon::app()
-        .addListener("0.0.0.0", 8080)
-        .setDocumentRoot("../")
-        .run();
+    drogon::app().run();
 }
